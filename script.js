c76bad75e62ef09e045a0f0523da9312cc5d661b
Nishitha-Yatham <120168184+Nishitha-Yatham@users.noreply.github.com>
2023-02-16 22:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+var btn =document.getElementById("submit");
+function minCostToFormRope(a) {
+  a.preventDefault();
+  var inputElement = document.querySelector("input").value;
+  var arr = inputElement.split(",");
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+
+ 
+  var cost = 0;
+  while (arr.length > 1) {
+    var res = Number(arr[0]) + Number(arr[1]);
+    arr.splice(0, 2);
+
+    arr.push(res);
+    cost += res;
+
+    arr.sort(function (a, b) {
+      return a - b;
+    });
+  }
+
+  document.getElementById("result").textContent = cost;
+}
+btn.addEventListener('click',minCostToFormRope(a))
